28abc82fd20a82e8bd0ae1ba0b755bf14cec5024
anupavanm <anupavanm@gmail.com>
2016-05-26 12:49:37 -0500

    Add files via upload
    Home work 4

---
+++ HomeWork4/HomeWork4/Employee.cs
@@ -0,0 +1,48 @@
+﻿//create the employee class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    //class declaration
+    class Employee
+    {
+        //propert to store firstName
+        public string FirstName { get; set; }
+
+        //propert to store LastName
+        public string LastName { get; set; }
+
+        private decimal salary;
+
+        //property to store Salary
+        public decimal Salary {
+
+            get
+            {
+                return salary;
+            }
+
+            set
+            {
+                //set the salary only if value is positive
+                if (value >= 0)
+                {
+                    salary = value;
+                }
+            }
+        
+        }
+
+        public Employee(string fname, string lname, double money)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Salary = Convert.ToDecimal(money);
+        }
+
+    }
+}
---
+++ HomeWork4/HomeWork4/EmployeeTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            //create two instance of Employee class
+            Employee emp1 = new Employee("Anupavan", "Muddana", 80000);
+
+            Employee emp2 = new Employee("Pavan", "Bhatraju", 85000);
+
+            Console.WriteLine("Yearly salary of {0} {1} is : {2}",
+                              emp1.FirstName,
+                              emp1.LastName,
+                              emp1.Salary);
+
+            Console.WriteLine("Yearly salary of {0} {1} is : {2}",
+                  emp2.FirstName,
+                  emp2.LastName,
+                  emp2.Salary);
+
+            emp1.Salary = (emp1.Salary * Convert.ToDecimal(1.1) );
+            emp2.Salary = (emp2.Salary * Convert.ToDecimal(1.1));
+
+
+            Console.WriteLine("Modified Yearly salary of {0} {1} is : {2}",
+                  emp1.FirstName,
+                  emp1.LastName,
+                  emp1.Salary);
+
+            Console.WriteLine("Modified Yearly salary of {0} {1} is : {2}",
+                  emp2.FirstName,
+                  emp2.LastName,
+                  emp2.Salary);
+
+            Console.ReadLine();
+
+        }
+    }
+}
