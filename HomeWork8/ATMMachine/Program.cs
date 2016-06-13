c522776c4d0d06921d99ee1df5e8217be7ba13fd
anupavanm <anupavanm@gmail.com>
2016-06-13 17:08:24 -0500

    Homework8 commit

---
+++ HomeWork8/ATMMachine/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ATMMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account[] accounts = new Account[10];
+
+            for (int i = 0; i <= 9; i++)
+            {
+                accounts[0] = new Account(i, 100);
+
+            }
+
+
+            int selectedOption;
+
+            int accountId;
+
+            do
+            {
+                Console.WriteLine("Please enter the account id: ");
+
+                accountId = Convert.ToInt32(Console.ReadLine());
+
+                if (accountId >= 0 && accountId <= 9)
+                {
+
+                    do
+                    {
+                        Console.WriteLine("{0}\n   {1}\n   {2}\n   {3}\n   {4}\n",
+
+                            "Please enter a choice:",
+                            "1 for viewing the current balance",
+                            "2 for withdrawing money",
+                            "3 for depositing money",
+                            "4 for exiting the main menu "
+
+                         );
+
+                        selectedOption = Convert.ToInt32(Console.ReadLine());
+
+
+
+                        if (selectedOption != 4)
+                        {
+                            Console.WriteLine("Seleted Operation Successfull. \n");
+                        }
+
+
+                    } while (selectedOption != 4);
+                }
+                else
+                {
+                    Console.WriteLine("you have selected an invalid Id");
+                }
+
+            } while (true);
+
+        }
+    }
+}
---
+++ HomeWork8/AirlineReservation/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace AirlineReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool[] seats = new[] { true, false, false, false, false, false, false, false, false, false, false };
+
+            int selectedType;
+
+            int nextAvailableEconomySeat = 6;
+
+            int nextAvailableFCSeat = 1;
+
+
+            do
+            {
+                //display messages to user for selection
+                Console.WriteLine("{0}\n    {1}\n    {2}", "Please type:", "1 for First Class and Please type ", "2 for Economy", "-1 to Quit");
+
+                selectedType = Convert.ToInt32(Console.ReadLine());
+
+                if (selectedType == 1)
+                {
+
+                    if (nextAvailableFCSeat <= 5)
+                    {
+                        seats[nextAvailableFCSeat] = true;
+
+                        Console.WriteLine("First Class Seat {0} Assigned.", nextAvailableFCSeat);
+
+                        nextAvailableFCSeat++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Currently we do not have any available tickets. Next flight leaves in 3 hours");
+                    }
+
+
+
+                }
+                else if (selectedType == 2)
+                {
+                    if (nextAvailableEconomySeat <= 10)
+                    {
+
+                        seats[nextAvailableEconomySeat] = true;
+
+                        Console.WriteLine("Economy Seat {0} Assigned.", nextAvailableEconomySeat);
+
+                        nextAvailableEconomySeat++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Currently we do not have any availble seats in Economy. Would you like to be assigned seat in FirstClass. ");
+
+                        Console.WriteLine("Press Y to assign a first class ticket or N to opt-out");
+
+                        if (Console.ReadLine().ToUpper().Equals("Y"))
+                        {
+
+                            if (nextAvailableFCSeat <= 5)
+                            {
+                                seats[nextAvailableFCSeat] = true;
+
+                                Console.WriteLine("First Class Seat {0} Assigned.", nextAvailableFCSeat);
+
+                                nextAvailableFCSeat++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Currently we do not have any available tickets. Next flight leaves in 3 hours");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Currently we do not have any available tickets. Next flight leaves in 3 hours");
+
+                            break;
+                        }
+                    }
+                }
+
+
+
+            } while (selectedType != -1);
+
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
